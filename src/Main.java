9363ff05c5d95e7b53b41c476e5589402de4896b
HWSanduni <sandushashikala96@gmail.com>
2020-08-02 22:34:31 +0530

    Create Dashbord and registraion Froms

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/lk/ijse/studentmanagementsystem/view/DashbordForm.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
-        System.out.println("Hello");
+
+
+
     }
 
 
---
+++ src/lk/ijse/studentmanagementsystem/controller/DashBordController.java
@@ -0,0 +1,28 @@
+package lk.ijse.studentmanagementsystem.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DashBordController {
+
+    public AnchorPane rootpane;
+
+    public void StudentOnMouseClicked(ActionEvent actionEvent) throws IOException {
+
+
+    }
+
+    public void RegistraionOnMouseClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/lk/ijse/studentmanagementsystem/view/RegistrationFrom.fxml"));
+        Scene scene = new Scene(root);
+        Stage mainStage = (Stage)(rootpane.getScene().getWindow());
+        mainStage.setScene(scene);
+        mainStage.centerOnScreen();
+    }
+}
