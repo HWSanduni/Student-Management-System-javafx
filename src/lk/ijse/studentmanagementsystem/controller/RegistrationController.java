7f9b68e6bd5df7bd64f076cb1bd5d715ba5d50d9
HWSanduni <sandushashikala96@gmail.com>
2020-08-03 20:24:10 +0530

    Create Exam Result Form And Exam Details fom Complete

--- src/lk/ijse/studentmanagementsystem/controller/RegistrationController.java
+++ src/lk/ijse/studentmanagementsystem/controller/RegistrationController.java
@@ -1,4 +1,22 @@
 package lk.ijse.studentmanagementsystem.controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class RegistrationController {
+    public AnchorPane root;
+
+    public void HomeOnActionCliced(MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/lk/ijse/studentmanagementsystem/view/DashbordForm.fxml"));
+        Scene scene = new Scene(root);
+        Stage mainStage = (Stage)(this.root.getScene().getWindow());
+        mainStage.setScene(scene);
+        mainStage.centerOnScreen();
+    }
 }
--- src/lk/ijse/studentmanagementsystem/controller/SubjectController.java
+++ src/lk/ijse/studentmanagementsystem/controller/SubjectController.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class BatchController {
+public class SubjectController {
 
     public AnchorPane root;
 
