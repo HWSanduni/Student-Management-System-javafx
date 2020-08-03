caade2a1b00b6c6bceacf8c3ccf4e2378c160631
HWSanduni <sandushashikala96@gmail.com>
2020-08-03 16:44:21 +0530

    Create Exam Ui And Payment Ui Complete

--- src/lk/ijse/studentmanagementsystem/controller/DashBordController.java
+++ src/lk/ijse/studentmanagementsystem/controller/DashBordController.java
@@ -49,4 +49,12 @@ public class DashBordController {
         mainStage.setScene(scene);
         mainStage.centerOnScreen();
     }
+
+    public void ExamOnMouseClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/lk/ijse/studentmanagementsystem/view/ExamForm.fxml"));
+        Scene scene = new Scene(root);
+        Stage mainStage = (Stage)(rootpane.getScene().getWindow());
+        mainStage.setScene(scene);
+        mainStage.centerOnScreen();
+    }
 }
