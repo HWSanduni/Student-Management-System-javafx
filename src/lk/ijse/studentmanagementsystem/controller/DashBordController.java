62d8918422b252ea9ee84a03d9a8494f312a9492
HWSanduni <sandushashikala96@gmail.com>
2020-08-03 13:47:03 +0530

    Create Batch ui And Complete

--- src/lk/ijse/studentmanagementsystem/controller/DashBordController.java
+++ src/lk/ijse/studentmanagementsystem/controller/DashBordController.java
@@ -25,4 +25,12 @@ public class DashBordController {
         mainStage.setScene(scene);
         mainStage.centerOnScreen();
     }
+
+    public void BatchOnMouseClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/lk/ijse/studentmanagementsystem/view/BatchForm.fxml"));
+        Scene scene = new Scene(root);
+        Stage mainStage = (Stage)(rootpane.getScene().getWindow());
+        mainStage.setScene(scene);
+        mainStage.centerOnScreen();
+    }
 }
