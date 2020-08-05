bccf990f35933cd914dd59efca6eaaf28f87d59a
HWSanduni <sandushashikala96@gmail.com>
2020-08-05 19:38:18 +0530

    Create ExamDetails Entity and EntityPk

---
+++ src/lk/ijse/studentmanagementsystem/entity/ExamDetailsPK.java
@@ -0,0 +1,40 @@
+package lk.ijse.studentmanagementsystem.entity;
+
+public class ExamDetailsPK {
+
+    private String examId;
+    private String coruseId;
+
+
+    public ExamDetailsPK() {
+    }
+
+    public ExamDetailsPK(String examId, String coruseId) {
+        this.examId = examId;
+        this.coruseId = coruseId;
+    }
+
+    public String getExamId() {
+        return examId;
+    }
+
+    public void setExamId(String examId) {
+        this.examId = examId;
+    }
+
+    public String getCoruseId() {
+        return coruseId;
+    }
+
+    public void setCoruseId(String coruseId) {
+        this.coruseId = coruseId;
+    }
+
+    @Override
+    public String toString() {
+        return "ExamDetailsPK{" +
+                "examId='" + examId + '\'' +
+                ", coruseId='" + coruseId + '\'' +
+                '}';
+    }
+}
