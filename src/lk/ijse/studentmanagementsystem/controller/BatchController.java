b9ef89e8811b6d2cf2155a08f9ae70cce56b17ed
HWSanduni <sandushashikala96@gmail.com>
2020-08-06 15:01:11 +0530

    Load All Batch

--- src/lk/ijse/studentmanagementsystem/controller/BatchController.java
+++ src/lk/ijse/studentmanagementsystem/controller/BatchController.java
@@ -1,17 +1,74 @@
 package lk.ijse.studentmanagementsystem.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.studentmanagementsystem.business.BOFactroy;
+import lk.ijse.studentmanagementsystem.business.BOType;
+import lk.ijse.studentmanagementsystem.business.custom.BatchBO;
+import lk.ijse.studentmanagementsystem.util.BatchTM;
 
 import java.io.IOException;
+import java.util.List;
 
 public class BatchController {
 
+
     public AnchorPane root;
+    public TextField txtBatchId;
+    public TextField txtBatchName;
+    public TextField txtCourseName;
+    public TextField txtBatchType;
+    public TextField txtYear;
+    public Button btnSave;
+    public Button btnRemove;
+    public DatePicker picStartDate;
+    public DatePicker picEndDate;
+    public TableView<BatchTM> tblBatch;
+
+    BatchBO batchBO = BOFactroy.getInstance().getBO(BOType.BATCH);
+
+    public void initialize() {
+        tblBatch.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("Bid"));
+        tblBatch.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblBatch.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("type"));
+        tblBatch.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("year"));
+        tblBatch.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("startDate"));
+        tblBatch.getColumns().get(5).setCellValueFactory(new PropertyValueFactory<>("endDate"));
+        tblBatch.getColumns().get(6).setCellValueFactory(new PropertyValueFactory<>("courseId"));
+
+        loadAllBatch();
+    }
+
+    private void loadAllBatch() {
+        try {
+
+            tblBatch.getItems().clear();
+            List<BatchTM> batchTMList = null;
+            try {
+                batchTMList = batchBO.getAllBatch();
+                System.out.println(batchTMList);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ObservableList<BatchTM> batchTMS = FXCollections.observableArrayList(batchTMList);
+            tblBatch.setItems(batchTMS);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public void HomeOnActionCliced(MouseEvent mouseEvent) throws IOException {
 
